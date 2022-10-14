ee66889bd62aa9dff6259bd0a58215643d47851c
SergeyGribkov <114168915+SergeyGribkov@users.noreply.github.com>
2022-10-14 21:10:22 +0300

    Add files via upload

---
+++ Класс Person.py
@@ -0,0 +1,18 @@
+class Person:
+    fullName=None
+    age=None
+    def __init__ (self):
+        i=None
+    def __init__ (self, fullName='123' , age='3'):
+        self.age
+        self.fullName=fullName
+    def move (self):
+        print('Какой-то Person говорит ')
+    def talk(self):
+        print('Какой-то Person говорит ')
+    def __str__(self):
+        return self.fullName+' ' + self.age
+a=Person
+b=Person('Sergey' '53')
+print(a)
+print(b)
