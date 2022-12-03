29530ef94d716260f24a11cae687d9b05fde112a
SergeyGribkov <114168915+SergeyGribkov@users.noreply.github.com>
2022-12-03 19:58:42 +0300

    Add files via upload

---
+++ Queue.py
@@ -0,0 +1,18 @@
+a=int(input())
+Masiw =a.split(' ')
+class Queue:
+    Masiw=None
+    size=len(Masiw)
+    def __init__(self):
+        i=None
+    def __init__(self , Masiw):
+        self.Masiw=Masiw
+    def peek(self):
+        return Masiw[last]
+    def Enqueue(self):
+        return Masiw.append(obj)
+    def Dequeue(self):
+        return Masiw.pop(Masiw[0])
+queue=new_Queue
+queue.push(123)
+obj=queue.pop()
---
+++ Stack.py
@@ -0,0 +1,20 @@
+a=int(input())
+Masiw =a.split(' ')
+class Stack:
+    Masiw=None
+    size=len(Masiw)
+    def __init__(self):
+        i=None
+    def __init__(self , Masiw):
+        self.Masiw=Masiw
+    def peek(self):
+        return Masiw[last]
+    def pop(self):
+        last=last-1
+        return Masiw[last]
+    def push (self , obj):
+        Masiw.append(obj)
+        last=lst+1
+stack=new_Stack
+stack.push(123)
+obj=stack.pop()
